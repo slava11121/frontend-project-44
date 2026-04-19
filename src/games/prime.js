c22b16534612044d5b08ff898bd5c5bb44c4a2f3
Slava <Slava@MacBook-Pro-mac.local>
2026-04-19 12:14:37 +0300

    add prime game

---
+++ src/games/prime.js
@@ -0,0 +1,32 @@
+import runGame from '../index.js';
+
+const gameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const getRandomNumber = (min = 1, max = 100) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(number); i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const getQuestionAndAnswer = () => {
+  const number = getRandomNumber();
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
+
+  return [String(number), correctAnswer];
+};
+
+const runPrimeGame = () => {
+  runGame(gameRule, getQuestionAndAnswer);
+};
+
+export default runPrimeGame;
